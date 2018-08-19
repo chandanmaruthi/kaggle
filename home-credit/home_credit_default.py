8691d19f2386687fa67c31976fcda6e443553e1b
chandan maruthi <chandan.maruthi@gmail.com>
2018-08-19 15:04:33 -0700

    multiprocess

--- home-credit/home_credit_default.py
+++ home-credit/home_credit_default.py
@@ -13,6 +13,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import ExtraTreesClassifier
 import warnings
 from tqdm import tqdm
+from multiprocessing import Process
+
 
 warnings.filterwarnings("ignore")
 
@@ -52,7 +54,7 @@ class util:
                     oh_count +=1
                     #print('ce :' + col)
                     oh_df = pd.concat([oh_df,pd.get_dummies(df[col].astype(str),prefix=col+'_')],axis=1)
-                    df.drop([col], axis=1, inplace=True)
+                df.drop([col], axis=1, inplace=True)
 
         print('%d columns were label encoded.' % le_count)
         print('%d columns were one hot encoded.' % oh_count)
@@ -63,7 +65,8 @@ class util:
     def add_missing_dummy_columns( d, columns ):
         missing_cols = set( columns ) - set( d.columns )
         for c in missing_cols:
-            d[c] = 0
+            if c != 'TARGET':
+                d[c] = 0
         return d
 
     def handle_nulls(df):
@@ -98,7 +101,7 @@ class util:
         #predictions = rf_model.predict(test_features)
         #accuracy =  compute_score(rf_model,test_features,test_labels)
         #print(accuracy)
-        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:,1])
+        #false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:,1])
         #print(auc(false_positive_rate, true_positive_rate))
         score = roc_auc_score(y_test, model.predict(X_test))
         return score
@@ -155,68 +158,61 @@ class Models:
         util.record_model("xgb_1", xgb_model, accuracy)
 
     def model_lgbm(X,X_test, y, y_test):
-        categorical_features = [c for c, col in enumerate(X.columns) if 'cat' in col]
+        print(X.shape)
+        print(X_test.shape)
+        X.fillna(0, inplace=True)
+        X_test  .fillna(0, inplace=True)
+        y.fillna(0, inplace=True)
+        y_test.fillna(0,inplace = True)
+        from sklearn.preprocessing import StandardScaler
+        sc = StandardScaler()
+        X = sc.fit_transform(X)
+        X_test = sc.transform(X_test)
+        params = {}
+        categorical_features=[]
+        #categorical_features = [c for c, col in enumerate(X.columns) if 'cat' in col]
         train_data = lgbm.Dataset(X, label=y, categorical_feature=categorical_features)
         test_data = lgbm.Dataset(X_test, label=y_test)
 
-        params = {'boosting_type': 'gbdt',
-                  'max_depth': -1,
-                  'objective': 'binary',
-                  'nthread': 3,  # Updated from nthread
-                  'num_leaves': 64,
-                  'learning_rate': 0.05,
-                  'max_bin': 512,
-                  'subsample_for_bin': 200,
-                  'subsample': 1,
-                  'subsample_freq': 1,
-                  'colsample_bytree': 0.8,
-                  'reg_alpha': 5,
-                  'reg_lambda': 10,
-                  'min_split_gain': 0.5,
-                  'min_child_weight': 1,
-                  'min_child_samples': 5,
-                  'scale_pos_weight': 1,
-                  'num_class': 1,
-                  'metric': 'auc'}
-        # Create parameters to search
-        gridParams = {
-            'learning_rate': [0.005],
-            'n_estimators': [40],
-            'num_leaves': [6, 8, 12, 16],
-            'boosting_type': ['gbdt'],
-            'objective': ['binary'],
-            'random_state': [501],  # Updated from 'seed'
-            'colsample_bytree': [0.65, 0.66],
-            'subsample': [0.7, 0.75],
-            'reg_alpha': [1, 1.2],
-            'reg_lambda': [1, 1.2, 1.4],
-        }
 
-        model = lgbm.LGBMClassifier(boosting_type='gbdt',
-                                    objective='binary',
-                                    n_jobs=3,  # Updated from 'nthread'
-                                    silent=True,
-                                    max_depth=params['max_depth'],
-                                    max_bin=params['max_bin'],
-                                    subsample_for_bin=params['subsample_for_bin'],
-                                    subsample=params['subsample'],
-                                    subsample_freq=params['subsample_freq'],
-                                    min_split_gain=params['min_split_gain'],
-                                    min_child_weight=params['min_child_weight'],
-                                    min_child_samples=params['min_child_samples'],
-                                    scale_pos_weight=params['scale_pos_weight'])
+        # # Create parameters to search
+        # gridParams = {
+        #     'learning_rate': [0.005],
+        #     'n_estimators': [40],
+        #     'num_leaves': [6, 8, 12, 16],
+        #     'boosting_type': ['gbdt'],
+        #     'objective': ['binary'],
+        #     'random_state': [501],  # Updated from 'seed'
+        #     'colsample_bytree': [0.65, 0.66],
+        #     'subsample': [0.7, 0.75],
+        #     'reg_alpha': [1, 1.2],
+        #     'reg_lambda': [1, 1.2, 1.4],
+        # }
+        #
+        # model = lgbm.LGBMClassifier(boosting_type='gbdt',
+        #                             objective='binary',
+        #                             n_jobs=3,  # Updated from 'nthread'
+        #                             silent=True,
+        #                             max_depth=params['max_depth'],
+        #                             max_bin=params['max_bin'],
+        #                             subsample_for_bin=params['subsample_for_bin'],
+        #                             subsample=params['subsample'],
+        #                             subsample_freq=params['subsample_freq'],
+        #                             min_split_gain=params['min_split_gain'],
+        #                             min_child_weight=params['min_child_weight'],
+        #                             min_child_samples=params['min_child_samples'],
+        #                             scale_pos_weight=params['scale_pos_weight'])
+        #
+        # grid = GridSearchCV(model, gridParams,
+        #                     verbose=1,
+        #                     cv=4,
+        #                     n_jobs=2)
+        #
+        # # Run the grid
+        # grid.fit(X, y)
+        # params = grid.best_params_
 
-        grid = GridSearchCV(model, gridParams,
-                            verbose=1,
-                            cv=4,
-                            n_jobs=2)
 
-        # Run the grid
-        grid.fit(X, y)
-        params = grid.best_params_
-        #print(grid.best_params_)
-        #print(grid.best_score_)
-        # exit()
         # def prepLGB(data,
         #             classCol='',
         #             IDCol='',
@@ -246,13 +242,20 @@ class Models:
         #
         #     return lData, labels, IDs, data
         #
-        # params = {'boosting_type': 'gbdt', 'colsample_bytree': 0.65, 'learning_rate': 0.005, 'n_estimators': 40, 'num_leaves': 6,
-        #  'objective': 'binary', 'random_state': 501, 'reg_alpha': 1, 'reg_lambda': 1, 'subsample': 0.7}
+        # Feature Scaling
+
+        params['learning_rate'] = 0.003
+        params['boosting_type'] = 'gbdt'
+        params['objective'] = 'binary'
+        params['metric'] = 'binary_logloss'
+        params['sub_feature'] = 0.5
+        params['num_leaves'] = 10
+        params['min_data'] = 50
+        params['max_depth'] = 10
+
         model = lgbm.train(params,
                            train_data,
-                           num_boost_round=10000,
-                           valid_sets=[train_data, test_data],
-                           early_stopping_rounds=500,
+                           valid_sets=[test_data],
                            verbose_eval=-1)
         util.record_model("lgbm_1", model, 10)
 
@@ -469,16 +472,28 @@ def featureSelection(df, df_test):
 
 def run(df, df_test, df_all, train=True):
 
+    df = align_datasets(df,df_test)
+    df_test = align_datasets(df_test,df)
+    print(df.shape)
+    print(df_test.shape)
 
     df_raw= df
     df_test_raw= df_test
     X_train, X_test, y_train, y_test = util.gen_train_test_split(df)
 
-    # Models.model_rf(X_train, X_test,y_train, y_test)
-    # Models.model_gbm(X_train, X_test, y_train, y_test)
-    # Models.model_xgb_2(X_train, X_test, y_train, y_test)
+    X_train.fillna(0, inplace=True)
+    X_test.fillna(0, inplace=True)
+    df_test.fillna(0,inplace=True)
+
+    #y_train.fillna(0, inplace=True)
+    #y_test.fillna(0, inplace=True)
+
+
+    Models.model_rf(X_train, X_test,y_train, y_test)
+    Models.model_gbm(X_train, X_test, y_train, y_test)
+    Models.model_xgb_2(X_train, X_test, y_train, y_test)
     #Models.model_lgbm(X_train_raw, X_test_raw, y_train_raw, y_test_raw)
-    Models.model_lgbm(X_train, X_test, y_train, y_test)
+    #Models.model_lgbm(X_train, X_test, y_train, y_test)
     score = 0
     selected_model = None
     print(models)
@@ -487,7 +502,7 @@ def run(df, df_test, df_all, train=True):
             name = model_inst['model_name']
             score = model_inst['score']
             selected_model = model_inst['model']
-        print(str(score) + '_' + name)
+        print(name + " :  " + str(score))
 
     #prediction = selected_model.predict(df_test_raw.drop(['TARGET'],axis=1))
     prediction = selected_model.predict(df_test)
@@ -501,8 +516,10 @@ def make_subset(df, subset=0):
         df = df[:subset]
     return df
 
-
-
+df_application = None
+df_application_test = None
+arr_in_process_cols=[]
+arr_in_process_cols_test=[]
 
 def prep():
     # load data
@@ -515,53 +532,64 @@ def prep():
     df_application = make_subset(df_application, subset)
     df_application_test = make_subset(df_application_test, subset)
     print("{},{}".format(df_application.shape, df_application_test.shape))
+    print(df_application['TARGET'].unique())
+
     # Clean up data
     df_application = clean_up(df_application)
     df_application_test =  clean_up(df_application_test)
     print("{},{}".format(df_application.shape, df_application_test.shape))
+    print(df_application['TARGET'].unique())
     # Feature Engineer
     df_application = fe(df_application, df_application_test)
     df_application_test = fe(df_application_test, df_application)
     print("{},{}".format(df_application.shape, df_application_test.shape))
+    print(df_application['TARGET'].unique())
 
-    df_application =  get_agg_numerics_data(df_application, pd.read_csv('application_train.csv'),['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('application_test.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
+    parallel_funcs =[]
 
-    df_application =  get_agg_numerics_data(df_application, pd.read_csv('installments_payments.csv'),['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('installments_payments.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('application_train.csv'),['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('application_test.csv'), ['SK_ID_CURR'],False))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('installments_payments.csv'),['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('installments_payments.csv'), ['SK_ID_CURR'],False))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('bureau.csv'),['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('bureau.csv'), ['SK_ID_CURR'],False))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'],False))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('previous_application.csv'), ['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('previous_application.csv'), ['SK_ID_CURR'],False))
+    parallel_funcs.append(get_agg_numerics_data(df_application, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'],True))
+    parallel_funcs.append(get_agg_numerics_data(df_application_test, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'],False))
 
-    df_application =  get_agg_numerics_data(df_application, pd.read_csv('bureau.csv'),['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('bureau.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
+    runInParallel(parallel_funcs)
 
-    # df_application = get_agg_numerics_data(df_application, pd.read_csv('bureau_balance.csv'), ['SK_ID_CURR'])
-    # print("{},{}".format(df_application.shape, df_application_test.shape))
-
-    df_application = get_agg_numerics_data(df_application, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
-
-    df_application = get_agg_numerics_data(df_application, pd.read_csv('previous_application.csv'), ['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('previous_application.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
-
-    df_application = get_agg_numerics_data(df_application, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'])
-    df_application_test = get_agg_numerics_data(df_application_test, pd.read_csv('POS_CASH_balance.csv'), ['SK_ID_CURR'])
-    print("{},{}".format(df_application.shape, df_application_test.shape))
-
+    arr_in_process_cols.append(df_application)
+    arr_in_process_cols_test.append(df_application_test)
+    df_application = pd.concat(arr_in_process_cols, axis=0)
+    df_application_test = pd.concat(arr_in_process_cols_test, axis=0)
     # Align Datasets
     df_application = align_datasets(df_application,df_application_test)
-    df_application = align_datasets(df_application_test,df_application)
+    df_application_test = align_datasets(df_application_test,df_application)
     print("{},{}".format(df_application.shape, df_application_test.shape))
+    print(df_application['TARGET'].unique())
 
     # Checkpoint Save
-    df_application.to_csv('application_processed.csv')
-    df_application_test.to_csv('application_test_processed.csv')
+    df_application.to_csv('application_processed.csv',index = False)
+    df_application_test.to_csv('application_test_processed.csv',index = False)
     print("{},{}".format(df_application.shape, df_application_test.shape))
 
-def get_agg_numerics_data(df, additional_data, exclude_cols):
+
+def runInParallel(*fns):
+  proc = []
+  for fn in fns:
+    p = Process(target=fn)
+    p.start()
+    proc.append(p)
+  for p in proc:
+    p.join()
+
+#runInParallel(func1, func2)
+
+def get_agg_numerics_data(df, additional_data, exclude_cols, train= True):
     numeric_columns = []
     for col in additional_data.columns:
         if str(additional_data[col].dtype) not in ['object','category'] and col not in exclude_cols:
@@ -586,8 +614,10 @@ def get_agg_numerics_data(df, additional_data, exclude_cols):
                                   on=groupby_cols,
                                   how='left')
             groupby_aggregate_names.append(groupby_aggregate_name)
-    return df
-
+    if train:
+        arr_in_process_cols.append(df[groupby_aggregate_names])
+    else:
+        arr_in_process_cols_test.append(df[groupby_aggregate_names])
 
 def load_data():
     df_application = pd.read_csv('application_train.csv')
@@ -601,14 +631,21 @@ def load_data():
     return df_application, df_application_test
     # Feature Selection
     #df_application, df_application_test = featureSelection(df_application, df_application_test)
-#prep()
-#exit()
-
 
 
 prep()
+exit()
 
-#df = pd.read_csv('application_processed.csv')
-#df_test = pd.read_csv('application_test_processed.csv')
-#all_data = pd.concat([df, df_test],axis=0)
-#run(df,df_test,all_data, True)
+
+
+
+
+df = pd.read_csv('application_processed.csv')
+df_test = pd.read_csv('application_test_processed.csv')
+
+print(set(df.columns)-set(df_test.columns))
+
+all_data = pd.concat([df, df_test],axis=0)
+print(df['TARGET'].unique())
+
+run(df,df_test,all_data, True)
